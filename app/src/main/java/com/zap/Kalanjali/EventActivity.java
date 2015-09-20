8ff4e63ffa7aeb124d4d910aa054d749fe6f9d87
Zeeshan <zeetherocker@gmail.com>
2015-09-21 01:17:31 +0530

    Some UI Improvements1

--- app/src/main/java/com/zap/Kalanjali/EventActivity.java
+++ app/src/main/java/com/zap/Kalanjali/EventActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class EventActivity extends AppCompatActivity {
 
